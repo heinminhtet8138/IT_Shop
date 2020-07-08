2f0838f7c1131e1984aa2afea91ce2711ee58021
heinminhtet8138 <heinminhtet8138@gmail.com>
2020-07-08 19:34:47 +0630

    change custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+// Count Qty
 function count(){
 	var shop_str=localStorage.getItem("it_shop");
 	
@@ -11,6 +12,28 @@ function count(){
 				console.log(count);
 				$("#count").text(count);
 			})
+		}else{
+			$("#count").text('');
+		}
+	}else{
+		$("#count").text('');
+
+	}
+
+}
+
+// Count item
+function item_count(){
+	var shop_str=localStorage.getItem("it_shop");
+	
+	if (shop_str) {
+
+		var shop_arr=JSON.parse(shop_str);
+		if (shop_arr!=0) {
+			var count=shop_arr.length;
+				
+				console.log(count);
+				$("#count").text(count);
 		}else{
 			$("#count").text('');
 		}
